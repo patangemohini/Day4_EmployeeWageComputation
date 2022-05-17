d5eb98bd2adfde5652c6ca2b418a93539a99917f
mohini patange <patangemohini5@gmail.com>
2022-05-17 16:28:21 +0530

    Added : Employee is prsent or absent

--- src/employeewage/EmployeeWage.java
+++ src/employeewage/EmployeeWage.java
@@ -1,7 +1,13 @@
 package employeewage;
 
 public class EmployeeWage {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation!!!!");
+        double random = Math.floor(Math.random() * 10) % 2;
+        if (random == 1) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent ");
+        }
     }
 }

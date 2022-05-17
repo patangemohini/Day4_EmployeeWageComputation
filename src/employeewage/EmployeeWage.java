b3e7780937b6d9119bfca1438703ebeadd28fd1e
mohini patange <patangemohini5@gmail.com>
2022-05-17 17:06:17 +0530

    Added : Calculated wages till hours or days

--- src/employeewage/EmployeeWage.java
+++ src/employeewage/EmployeeWage.java
@@ -1,30 +1,41 @@
 package employeewage;
 
 public class EmployeeWage {
+
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
     static final int PART_TIME = 4;
 
     public static void main(String[] args) {
+
         int daily_wage = 0;
+        int hours = 0;
+        int days = 0;
 
-        System.out.println("Welcome to employee wage computation program");
+        System.out.println("Welcome to Employee Wage Computation Program!");
 
-        for (int i = 0; i <= 20; i++) {
+        while (days < 20 && hours < 100) {
             double empCheck = Math.floor(Math.random() * 10) % 3;
             switch ((int) empCheck) {
                 case 0:
                     System.out.println("Employee is present");
                     daily_wage = daily_wage + (WAGE_PER_HOUR * FULL_DAY_HOUR);
                     System.out.println("Daily wage is : " + daily_wage);
+                    hours = hours + FULL_DAY_HOUR;
+                    days++;
+                    System.out.println("full time working Hours " + hours);
                     break;
                 case 1:
                     System.out.println("Employee is present");
                     daily_wage = daily_wage + (WAGE_PER_HOUR * PART_TIME);
                     System.out.println("part time wage is : " + daily_wage);
+                    hours = hours + PART_TIME;
+                    days++;
+                    System.out.println("part time working hours : " + hours);
                     break;
-                default:
-                    System.out.println("Employee is absent ");
+                case 2:
+                    System.out.println("Employee is Absent");
+                    break;
             }
         }
     }

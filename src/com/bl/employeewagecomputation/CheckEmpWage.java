7612cbfe7d44dc7bd68aa410c902a717887384b6
mohini patange <patangemohini5@gmail.com>
2022-05-30 22:47:46 +0530

    Added : employee wage for multiple companies

--- src/com/bl/employeewagecomputation/CheckEmpWage.java
+++ src/com/bl/employeewagecomputation/CheckEmpWage.java
@@ -1,42 +1,47 @@
 package com.bl.employeewagecomputation;
 
 public class CheckEmpWage {
-    static final int WAGE_PER_HOUR = 20;
-    static final int FULL_DAY_HOUR = 8;
-    static final int PART_TIME = 4;
-    static final int TOTAL_WORKING_HOURS = 100;
-    static final int EMP_PRESENT = 0;
-    static final int EMP_PART_TIME = 1;
-    static final int EMP_ABSENT = 2;
+    int FULL_DAY_HOUR = 8;
+    int PART_TIME = 4;
+    int daily_wage = 0;
+    int hours = 0;
+    int days = 0;
+    int totalWorkHours = 0;
 
-    public void checkAttendanceAndEmpWage() {
-        int dailyWage = 0;
-        int hours = 0;
-        int days = 0;
+    public int totalWorkingHours(int hour, int numWorkingHours) {
+        totalWorkHours += hour;
+        if (totalWorkHours > numWorkingHours) {
+            totalWorkHours -= hour;
+        }
+        return totalWorkHours;
+    }
 
-        while (days < WAGE_PER_HOUR && hours < TOTAL_WORKING_HOURS) {
+    public void checkAttendanceAndWage(String companyName, int wagePerHr, int numOfWorkingDays, int numWorkingHours) {
+
+        while (days < numOfWorkingDays && hours < numWorkingHours) {
             double empCheck = Math.floor(Math.random() * 10) % 3;
             switch ((int) empCheck) {
-                case EMP_PRESENT:
-                    System.out.println("Employee is present");
-                    dailyWage = dailyWage + (WAGE_PER_HOUR * FULL_DAY_HOUR);
-                    System.out.println("Daily wage is : " + dailyWage);
-                    hours = hours + FULL_DAY_HOUR;
+                case 0:
+                    System.out.println("Employee is present ");
+                    daily_wage = daily_wage + (wagePerHr * FULL_DAY_HOUR);
+                    System.out.println("Daily wage is : " + daily_wage);
+                    hours = totalWorkingHours(FULL_DAY_HOUR, numWorkingHours);
+                    System.out.println("Full time working hours : " + hours);
                     days++;
-                    System.out.println("full time working Hours " + hours);
                     break;
-                case EMP_PART_TIME:
-                    System.out.println("Employee is present");
-                    dailyWage = dailyWage + (WAGE_PER_HOUR * PART_TIME);
-                    System.out.println("part time wage is : " + dailyWage);
-                    hours = hours + PART_TIME;
+                case 1:
+                    System.out.println("Employee is present ");
+                    daily_wage = daily_wage + (wagePerHr * PART_TIME);
+                    System.out.println("Part time wage is : " + daily_wage);
+                    hours = totalWorkingHours(PART_TIME, numWorkingHours);
+                    System.out.println("Part time working hours : " + hours);
                     days++;
-                    System.out.println("part time working hours : " + hours);
                     break;
-                case EMP_ABSENT:
-                    System.out.println("Employee is Absent");
+                case 2:
+                    System.out.println("Employee is absent");
                     break;
             }
         }
+        System.out.println("Total employee wage of the " + companyName + " is :" + daily_wage);
     }
 }
--- src/com/bl/employeewagecomputation/EmployeeWage.java
+++ src/com/bl/employeewagecomputation/EmployeeWage.java
@@ -5,6 +5,7 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Using oops!");
         CheckEmpWage checkEmpWage = new CheckEmpWage();
-        checkEmpWage.checkAttendanceAndEmpWage();
+        checkEmpWage.checkAttendanceAndWage("Wipro", 20, 20, 100);
+        checkEmpWage.checkAttendanceAndWage("Infosys", 22, 25, 120);
     }
 }
